a448bafc180b44259dced7355cccb9440b83b7ab
elin202 <152214492+elin202@users.noreply.github.com>
2023-11-29 14:25:48 -0500

    Add files via upload

---
+++ finalapp.py
@@ -0,0 +1,25 @@
+import pandas as pd 
+import plotly.express as px
+import matplotlib.pyplot as plt
+import streamlit as st
+data = pd.read_csv("train.csv")
+fig1 = px.box(data,x="Credit_Score", 
+             y="Monthly_Inhand_Salary", 
+             color="Credit_Score",
+             title="Credit Scores Based on Monthly Inhand Salary", 
+             color_discrete_map={'Poor':'red',
+                                 'Standard':'orange',
+                                 'Good':'green'})
+fig.update_traces(quartilemethod="exclusive")
+
+fig2 = px.box(data,x="Credit_Score", 
+             y="Age", 
+             color="Credit_Score",
+             title="Credit Scores Based on Age", 
+             color_discrete_map={'Poor':'red',
+                                 'Standard':'orange',
+                                 'Good':'green'})
+fig.update_traces(quartilemethod="exclusive")
+st.title("Credit Scores Analysis")
+st.plotly_chart(fig1)
+st.plotly_chart(fig2)
